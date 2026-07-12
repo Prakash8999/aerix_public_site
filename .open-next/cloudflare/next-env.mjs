177732821367f7bb625be05d750771838bc1e627
Prakash8999 <jhaprakash504@gmail.com>
2026-07-12 15:33:56 +0530

    made changes

---
+++ .open-next/cloudflare/next-env.mjs
@@ -0,0 +1,3 @@
+export const production = {"NEXT_PUBLIC_EMAIL_API":"http://localhost:3001"};
+export const development = {"NEXT_PUBLIC_EMAIL_API":"http://localhost:3001"};
+export const test = {"NEXT_PUBLIC_EMAIL_API":"http://localhost:3001"};
